4fe8958e2d51cd2d96640d7adcb1505c7b3396d0
Trong Do <trongdoduc99@gmail.com>
2023-11-29 23:41:57 +0700

    Generate node graph to represent a platform where tower can be built and creep can navigate

--- Assets/_Project/Code/Scripts/AStar.cs
+++ Assets/_Project/Code/Scripts/AStar.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30809b796758a206efa9fa5f3c9a9f40163ebe8a878f0c14dd54caf75e7e6e32
-size 2883
+oid sha256:f9662143bb212e28aefd5dcad27554b781c7a2c57d57ae40da86efbcbf9d885f
+size 3780
--- Assets/_Project/Code/Scripts/BuildPlatform.cs
+++ Assets/_Project/Code/Scripts/BuildPlatform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b75d92df70e9d7fcc3942dd0c2aed6b7f2640e408704289a3e3fba35e578b0a
-size 297
+oid sha256:36e22df069b91ff6c4379dbb8f2ca1c601f4c9d44eaf9bb989efaf4958b2f2ef
+size 567
