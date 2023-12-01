912a074fa031a07512714234f16614777d4cb5ad
Trong Do <trongdoduc99@gmail.com>
2023-12-02 01:07:41 +0700

    Fix camera movement affected by the tilt of the pivot point

--- Assets/_Project/Code/Scripts/Camera/CameraControl.cs
+++ Assets/_Project/Code/Scripts/Camera/CameraControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65c1be8f2050462c0e61fdcbc1cf853c3c2fcaa9b270b22a3843edef4a6a6bdd
-size 2870
+oid sha256:4a18251c317c288e53ccc74b3f15a8458965d9c330fc7965c364f3bc0e50c2ff
+size 2838
