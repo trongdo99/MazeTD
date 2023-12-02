0e8b7d937783b18883859be797bcaef107ad66ff
Trong Do <trongdoduc99@gmail.com>
2023-12-03 01:16:33 +0700

    Change the logic to handle camera movement

--- Assets/_Project/Code/Scripts/Camera/CameraControl.cs
+++ Assets/_Project/Code/Scripts/Camera/CameraControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7f423dec3b0b3740a1491086334de7fb8147fc91207e51989fcb5e84fc65d5ca
-size 3075
+oid sha256:ea9e2c0da8ade2766bb123b5588bbc937c657132d6680c6e3fc08459596091a6
+size 3774
