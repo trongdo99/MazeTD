772e14b8edc8b1f7374480395390da3206e3c044
Trong Do <trongdoduc99@gmail.com>
2023-12-02 22:51:02 +0700

    Implement logic to move the preview tower to valid node on the platform

--- Assets/_Project/Code/Scripts/Managers/TowerManager.cs
+++ Assets/_Project/Code/Scripts/Managers/TowerManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af7191b7012c10ab7c2ae59b2869c163d993efd237020b6433b5be1c706ca4e9
-size 740
+oid sha256:083628b8eebb7cd39d1f45ecb7be3cfdfd92be05b94bfc5825513533438adda7
+size 2023
