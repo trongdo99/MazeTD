e8d832629602f482fc11f248162176342135d6d7
Trong Do <trongdoduc99@gmail.com>
2023-12-03 00:55:37 +0700

    Remove the multiplication to Time.Delta time for camera movement using mouse

--- Assets/_Project/Code/Scripts/Camera/CameraControl.cs
+++ Assets/_Project/Code/Scripts/Camera/CameraControl.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b0f7f25f18fae93994714ce4eafa86ae1a91c68497133272ec1315d0ade39a2
-size 3114
+oid sha256:7f423dec3b0b3740a1491086334de7fb8147fc91207e51989fcb5e84fc65d5ca
+size 3075
