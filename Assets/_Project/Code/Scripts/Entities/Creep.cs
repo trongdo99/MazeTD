d8fe15eedc303d830f77be73c280dba2e56921b4
Trong Do <trongdoduc99@gmail.com>
2023-12-03 19:26:22 +0700

    Creep rotate to face moving direction

--- Assets/_Project/Code/Scripts/Entities/Creep.cs
+++ Assets/_Project/Code/Scripts/Entities/Creep.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca1224c6ea531079e6dafea4227f6c7110778cffdb842336989fd288189e90d2
-size 978
+oid sha256:a8dc22f65ea38010be9d8023bf56e58c7e4b2cfb4ca5898dcfaf17f7ea449271
+size 1043
