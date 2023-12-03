a59045a0cab1026ac43d3dbdfa1d73c78b7ab8ca
Trong Do <trongdoduc99@gmail.com>
2023-12-03 20:10:38 +0700

    Update GetNeighbors method to consider diagonal blockage in AStar class

--- Assets/_Project/Code/Scripts/AStar.cs
+++ Assets/_Project/Code/Scripts/AStar.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf81d9dd82b4d1f4200e5c422cfe45e0eec6bb27e820ec7d8fd4e0d6bb899a78
-size 5804
+oid sha256:e8d24c0a979da31b278168485a24075225c486ed01b888f48d53c9c7fa34a289
+size 6216
