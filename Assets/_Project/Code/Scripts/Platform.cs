6afa597e82014fea2f10a4c4b7ca513bfb997615
Trong Do <trongdoduc99@gmail.com>
2023-12-03 20:52:01 +0700

    Prevent player from placing tower directly on entry point or end point

--- Assets/_Project/Code/Scripts/Platform.cs
+++ Assets/_Project/Code/Scripts/Platform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6146c1cc73a63b47d089266f9e55e4630cb3cdc80777202ccd77e0f1f8255b8d
-size 5713
+oid sha256:eba9553e81b5e777d136e223ecaaf5b99459725cf3c6838719c76f2e5fff9333
+size 5915
