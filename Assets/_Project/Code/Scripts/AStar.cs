86a866a189e0062d54bf29f7eeec726f6904da63
Trong Do <trongdoduc99@gmail.com>
2023-12-12 00:44:47 +0700

    Take into account of the platform rotation when generating node graph

--- Assets/_Project/Code/Scripts/AStar.cs
+++ Assets/_Project/Code/Scripts/AStar.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73c1aab78df9edab44ea14fcb4a4923105f24123ee47a6d0043a3db4c6b5e1da
-size 6264
+oid sha256:9734dfba06ae0472949d1b895dd56450abe24ee3ff8dbc976f17b8e46222786b
+size 6620
