d565572203049e890de6423cb2ab0f416627f73a
Trong Do <trongdoduc99@gmail.com>
2023-12-14 01:15:29 +0700

    Cache all paths in the level at when intialized

--- Assets/_Project/Code/Scripts/Managers/PathManager.cs
+++ Assets/_Project/Code/Scripts/Managers/PathManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2900597cad626c1f0390f0ef400dcb02e7dd77e004c33da59000f8563af14feb
-size 1514
+oid sha256:c007be0ec158610c474b21599932a3445a1d764b61cb02401f87d93a01c66b0d
+size 1652
