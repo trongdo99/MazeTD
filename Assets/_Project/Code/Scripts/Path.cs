18be6b41d273bc2cc8222cc028a2cde1d3cf37e4
Trong Do <trongdoduc99@gmail.com>
2023-12-14 00:59:35 +0700

    Calculate the distance of each path when initialized

--- Assets/_Project/Code/Scripts/Path.cs
+++ Assets/_Project/Code/Scripts/Path.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba4400d9d3c3033e57739af95f96b66ddebd5934041d242f0ff1906f0f917d3a
-size 3179
+oid sha256:7e0e5b3f0592a29e427460ce9030adf334c67f80c1c20062a517a1852b129274
+size 4735
--- Assets/_Project/Code/Scripts/Waypoint.cs
+++ Assets/_Project/Code/Scripts/Waypoint.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06ee644acc675d9926886992f16da069ce290e51e22f7f78a2b4965d0943721b
-size 240
+oid sha256:8e009f1a02de62d2760a8352309be00edf34c8fb79af665529ebe8e8838ae649
+size 1268
