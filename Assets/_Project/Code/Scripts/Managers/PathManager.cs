7559ac0154e129c2869fe676c778e67faa98da3b
Trong Do <trongdoduc99@gmail.com>
2023-12-14 23:16:55 +0700

    Calculate distance from all paths to end when initialized

--- Assets/_Project/Code/Scripts/Managers/PathManager.cs
+++ Assets/_Project/Code/Scripts/Managers/PathManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c007be0ec158610c474b21599932a3445a1d764b61cb02401f87d93a01c66b0d
-size 1652
+oid sha256:c13d1367014a99d74b4c02142c120fc108bcd8e814b80aa25c48cc8b435c20bf
+size 2481
