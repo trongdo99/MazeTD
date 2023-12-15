1db6f02e97a7a9488b298b4a5c6d71f3d88afbd4
Trong Do <trong.do-duc@ubisoft.com>
2023-12-15 17:51:15 +0700

    Fix GetNeighbors method throws index out of bound exception

--- Assets/_Project/Code/Scripts/AStar.cs
+++ Assets/_Project/Code/Scripts/AStar.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25a7a13ece2734defad68c8a4121d14d41b096619d1671a089bb8799d1d1f32d
-size 8130
+oid sha256:d86cd6dc2ec321edcfff031ae13fd2317c738036590d1f8681e2caf35fb017e9
+size 8610
