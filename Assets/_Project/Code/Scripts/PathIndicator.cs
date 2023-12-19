c34d42e017f535a240f3b5bbd15c084cd77e0d89
Trong Do <trongdoduc99@gmail.com>
2023-12-19 14:21:02 +0700

    Update path indicator whenever path changed

--- Assets/_Project/Code/Scripts/PathIndicator.cs
+++ Assets/_Project/Code/Scripts/PathIndicator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ef1cf8ed092882b3c7e8fb8aea8c032845f5cb74039232e23efa38eb52e7fd5
-size 1577
+oid sha256:23d76112243c6b1c4024c83e1c7627af0ca8205d6467fc42e80edb31d4bcb2fa
+size 1877
--- Assets/_Project/Code/Scripts/Platform.cs
+++ Assets/_Project/Code/Scripts/Platform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:172df38c1897211d3be63507330dcc29459f4310247e181d39ca75d43c6c69be
-size 6144
+oid sha256:955d7afcdd61a2ed3f0a6bb44652da6689822e93fdae85fb2ede345b7aa3073d
+size 6283
