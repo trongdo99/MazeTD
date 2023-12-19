292a8d23c92d0ae895c00c603b9412b028a11f35
Trong Do <trongdoduc99@gmail.com>
2023-12-19 12:30:47 +0700

    Include more node's information to help debugging

--- Assets/_Project/Code/Scripts/AStar.cs
+++ Assets/_Project/Code/Scripts/AStar.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0643a6e217115fa230f498b671ad09d496109e1977c8acac11aa1df95adaebfa
-size 8709
+oid sha256:bab6ece0585228b0a62de0e7d7f7e7a30bd8e603b2a0d334c247606f75c8ea14
+size 9535
--- Assets/_Project/Code/Scripts/Platform.cs
+++ Assets/_Project/Code/Scripts/Platform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48afeaf6ba47f0fc2dc4da0a951fd8cae3022f60de3a1ca1331d4cc633a917ee
-size 6301
+oid sha256:8101ea518c18cf888906502f2ca8964485a174c0368020d00380a0fca6c4520b
+size 6444
