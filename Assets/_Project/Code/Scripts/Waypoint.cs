3db2ff212dab8f05039eb6c95ef106c8c3edd641
Trong Do <trongdoduc99@gmail.com>
2023-12-19 14:53:51 +0700

    Remove unnecessary UpdateDistance called

--- Assets/_Project/Code/Scripts/Waypoint.cs
+++ Assets/_Project/Code/Scripts/Waypoint.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c24afeeaf06fd114d7db2d71c3edd06552d3f99206e5b5b1ce6eb4ab9a2d34a
-size 4990
+oid sha256:4bf1ebdf3429f976e059017513509d5d8b7a7f7d950c9c91b59888f741ab6a8f
+size 4943
