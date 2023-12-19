fc2e83dd223a9f669c7e1b26928abca70f9f873a
Trong Do <trongdoduc99@gmail.com>
2023-12-19 16:23:47 +0700

    There's a case after a enter preview mode, the selection info is null, add a null check to prevent it

--- Assets/_Project/Code/Scripts/Managers/TowerManager.cs
+++ Assets/_Project/Code/Scripts/Managers/TowerManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9369659f0fd9f182d2e47224c9bbb18120f14155d143a26cbe3355052fa12b48
-size 5131
+oid sha256:ad3b2943a6a7e10ae04557777a39aca2c187e4afdb38397e75f1b7c9ed87d1e1
+size 5157
