67267eae4b2c2ab7dd145623ec55cdae8da9eeb4
Trong Do <trongdoduc99@gmail.com>
2023-12-19 14:30:06 +0700

    Only check node when mouse cursor moved to another node

--- Assets/_Project/Code/Scripts/Managers/TowerManager.cs
+++ Assets/_Project/Code/Scripts/Managers/TowerManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:617a4e3621615b9a23485e27da2212ca6630c5a79e6e669b13be1803215aabf6
-size 5096
+oid sha256:a60bb007e6f464594497b938739ccb09e2c8efcf05c41fd6c97fdd854bbce41d
+size 5151
