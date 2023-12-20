b4c98f29bf8bcb1155f577f2bb7ee802427fdeac
Trong Do <trong.do-duc@ubisoft.com>
2023-12-20 10:44:35 +0700

    Create a list on Path to keep track of creeps on it

--- Assets/_Project/Code/Scripts/Entities/Creep.cs
+++ Assets/_Project/Code/Scripts/Entities/Creep.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e75c1fbbd16ab70e80db81ae5df7e6356a068cbceb3243f4f408bb8161259708
-size 2274
+oid sha256:774129139f4f25bf1018a2f3e2c7b50bb5d9046415fc40b9d9a70f52647d09c4
+size 2484
--- Assets/_Project/Code/Scripts/Path.cs
+++ Assets/_Project/Code/Scripts/Path.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d91d1bf82a3d45b66c8e8f2ca87dc2335ac4a26b41af5dea7e27649164a257c
-size 16424
+oid sha256:67ac06382662569b4408005d3e281d8773c9398ce72a06ac83e8f5d1e8115cae
+size 16725
