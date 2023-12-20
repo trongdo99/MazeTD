50f9076ef04f4f3587df801d7bee09ba0a90c2ba
Trong Do <trongdoduc99@gmail.com>
2023-12-20 20:38:51 +0700

    Fix index out of range exception when path is changed in a platform that has multiple exits

--- Assets/_Project/Code/Scripts/Entities/Creep.cs
+++ Assets/_Project/Code/Scripts/Entities/Creep.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6984e2fe123c797a6590fe45a6cdd8212df3265a79cb2ad6d32164aa64731f9c
-size 5795
+oid sha256:a7a56078b3865e21744e818dc9f92f7bd48a8fb39511beeadc5d367e02d49191
+size 5517
