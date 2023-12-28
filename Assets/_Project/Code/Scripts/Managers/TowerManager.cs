21023ea21df362e4f8b93be560ced5e5dab80a67
Trong Do <trongdoduc99@gmail.com>
2023-12-29 00:41:42 +0700

    Implement tower drag and build using Touch input

--- Assets/_Project/Code/Scripts/Managers/TowerManager.cs
+++ Assets/_Project/Code/Scripts/Managers/TowerManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c9731880c1e85dcfe528fbd795744d52101d1a2301b4576beb80d27b7987ff4
-size 5931
+oid sha256:e285a73b0492710da311bc480cc5c24113b781668a8b0fc9d0ddad073baf1ac8
+size 6529
