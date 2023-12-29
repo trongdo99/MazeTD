fbdeda20668e4052ca8fb225cf86e1a8ca58737f
Trong Do <trongdoduc99@gmail.com>
2023-12-29 21:10:22 +0700

    Fix compile error for mobile device

--- Assets/_Project/Code/Scripts/Utilities/FPSLimiter.cs
+++ Assets/_Project/Code/Scripts/Utilities/FPSLimiter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:048fdf242b562891e1792ece89ca8b9ddbeeccb244e905b1a4891aabb52be975
-size 627
+oid sha256:82a7c3a98fc972e9a284d52feccd4ab59e6da5ff303e689157a5b0e5896a18c8
+size 638
