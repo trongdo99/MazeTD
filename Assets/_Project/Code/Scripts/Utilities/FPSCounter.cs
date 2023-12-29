0d8e32e2af2e727c3fd20c401e9d42d91941fb45
Trong Do <trongdoduc99@gmail.com>
2023-12-29 19:44:37 +0700

    Add FPS counter and FPS limiter to project

---
+++ Assets/_Project/Code/Scripts/Utilities/FPSCounter.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:df81a210de58e1c725378f20c26c839809b223ac65de628aaad8c60187d88b6c
+size 1239
---
+++ Assets/_Project/Code/Scripts/Utilities/FPSLimiter.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:048fdf242b562891e1792ece89ca8b9ddbeeccb244e905b1a4891aabb52be975
+size 627
