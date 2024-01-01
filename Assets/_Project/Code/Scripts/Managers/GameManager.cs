5e8588042dac9d8237336a3a186056ed5b60c345
Trong Do <trongdoduc99@gmail.com>
2024-01-01 21:06:17 +0700

    Remove unused code

--- Assets/_Project/Code/Scripts/Managers/GameManager.cs
+++ Assets/_Project/Code/Scripts/Managers/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1179daab37fa7fcd3709f4b8fd7f7afba1eb87b2ed4510eb8b35c15a9b6a50f1
-size 461
+oid sha256:cdc04a0c3e2504f28066b709d2f9c2ebf7b765dc144c4cf3cba153d60f871f4b
+size 349
