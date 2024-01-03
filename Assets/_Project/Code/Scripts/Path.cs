58e117656e687ba421773ce1e0186db866946b33
Trong Do <trong.do-duc@ubisoft.com>
2024-01-03 14:09:44 +0700

    Fix issue when tower is removed, the path doesn't update at all

--- Assets/_Project/Code/Scripts/Path.cs
+++ Assets/_Project/Code/Scripts/Path.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e7db7445b06ddad7bfb815fcb7ea21c0985c477a01398a175c88643ab763f4c
-size 19280
+oid sha256:0d06424d37ede6a61f917c0a9f30f8c0a2c6d54c98037dc000c60d1b6725883f
+size 19325
--- Assets/_Project/Code/Scripts/Platform.cs
+++ Assets/_Project/Code/Scripts/Platform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa3fe477187b58e10e858b78c0cc3b9ec401db0b9199afd91eca6422d9c6e50f
-size 6583
+oid sha256:f789e9c9e458f8a6774d8045b379326e44e938c4a4363d38eb4fae8dcc32575e
+size 6578
