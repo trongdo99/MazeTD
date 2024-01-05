87af893271776c9e29db166752f62425a2db04c3
Trong Do <trong.do-duc@ubisoft.com>
2024-01-05 14:46:21 +0700

    Remove dead creep from the list in the path it was moving before died

--- Assets/_Project/Code/Scripts/Entities/Creep.cs
+++ Assets/_Project/Code/Scripts/Entities/Creep.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69e908e42e239c62587a93b54b9abf84fbf86a413839b977d15cada9999eb367
-size 7243
+oid sha256:823dd5012fd82f60e69d571be8e8a4c7abb93fcba05dbb1bf862dc5bbce26e55
+size 7464
