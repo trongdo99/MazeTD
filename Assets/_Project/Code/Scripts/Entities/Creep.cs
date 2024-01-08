6369a189ad326b2e74a4e1ed980591a8caa202c8
Trong Do <trong.do-duc@ubisoft.com>
2024-01-08 10:33:41 +0700

    Fix creep doesn't lose hp when being attacked

--- Assets/_Project/Code/Scripts/Entities/Creep.cs
+++ Assets/_Project/Code/Scripts/Entities/Creep.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58a7c8419278d93286d50916519fbc2241986c78e4a322fc334fba405f67da9e
-size 7535
+oid sha256:d7ff81237998d504b9f9602b765069ada508dff911978fa983b4b28fe5cea460
+size 7607
