ed7a787e9b1f05c40ced4078d7264e926a93fef4
Trong Do <trongdoduc99@gmail.com>
2024-01-08 19:46:37 +0700

    Check if tower use an AoE projectile using a boolean field

--- Assets/_Project/Code/Scripts/Entities/Tower.cs
+++ Assets/_Project/Code/Scripts/Entities/Tower.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af93f1c12de55dda99e8681a475b58ae89b006a1c9c11898798f231f3c6b6cd3
-size 10709
+oid sha256:e81807ed64225c3a81f889474d90cbcd88bc01060507d81753cb827b96d3328d
+size 10544
