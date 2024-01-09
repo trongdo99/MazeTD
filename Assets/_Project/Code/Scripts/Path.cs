4ab1baeaab5c5eca4d17e65663de2dde8e2d6b9f
Trong Do <trongdoduc99@gmail.com>
2024-01-09 22:07:59 +0700

    Remove TODO note

--- Assets/_Project/Code/Scripts/Path.cs
+++ Assets/_Project/Code/Scripts/Path.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d06424d37ede6a61f917c0a9f30f8c0a2c6d54c98037dc000c60d1b6725883f
-size 19325
+oid sha256:d9aed82f8489fe4e63645bdb65ec029888353310377c867a09fe6de88508e308
+size 19249
