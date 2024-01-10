276f624f76a56f0fa6ae1368f9c75ed214fdcbd7
Trong Do <trongdoduc99@gmail.com>
2024-01-11 00:20:08 +0700

    Working on creating Tesla tower (WIP)

--- Assets/_Project/Code/Scripts/Entities/Projectile.cs
+++ Assets/_Project/Code/Scripts/Entities/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b3ca2cb8f60616ff197fc0faea8d414b85a704a8221800831403bf27bd3ef51
-size 4144
+oid sha256:9816d0717e6c949960bf8e99d08e2cbdfe772fde512b2f7d5d6e470228386428
+size 7895
