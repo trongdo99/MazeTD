1c431f7908bee4440b95995cf8a1f76d639acb78
Trong Do <trongdoduc99@gmail.com>
2024-01-11 21:28:35 +0700

    Only use one Line Renderer to render Lightning projectile

--- Assets/_Project/Code/Scripts/Entities/Projectile.cs
+++ Assets/_Project/Code/Scripts/Entities/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da7d4d0db8efe15e702061768bc2ee6f64e2bd068850546ae14e6ef4cf06c1b6
-size 10275
+oid sha256:1bfbafbc22ace1a2a86dc20ba9d1527dbb37a9b10a2c6e38e68e8f9c5f23a397
+size 9221
