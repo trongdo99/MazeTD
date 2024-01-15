aa10786978fd5214ad52a9f97d75e2c6dc52d9f5
Trong Do <trongdoduc99@gmail.com>
2024-01-15 23:26:39 +0700

    Fix Tesla tower's projectile doesn't get destroyed if the target is null

--- Assets/_Project/Code/Scripts/Entities/Projectile.cs
+++ Assets/_Project/Code/Scripts/Entities/Projectile.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:023f603991df90ced529dc845c482272a68dbb7abc116770cffc1c20c0315ec7
-size 13310
+oid sha256:66a2590de525d55cf30dad3ba9cc9f7ee1fd569d63ccf41ddcec5a9b63884519
+size 13380
