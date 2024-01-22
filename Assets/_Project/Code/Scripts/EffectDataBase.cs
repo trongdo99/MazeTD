abf6dedfccb8d4c91942b732523625be526afe97
Trong Do <trong.do-duc@ubisoft.com>
2024-01-22 09:49:57 +0700

    - Add NaughtyAttributes package - Use Button attribute from NaughtyAttributes package to expose ResetId method to Inspector

--- Assets/_Project/Code/Scripts/EffectDataBase.cs
+++ Assets/_Project/Code/Scripts/EffectDataBase.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a8e71d50367d56b7d5157acd323ab8208d7301ed971b26ae9459d7c8713e2b1
-size 4762
+oid sha256:9ec5a629431b6bfd51ef6348b282855e9867380e064e92bf0bc553fc0cca91a4
+size 4604
