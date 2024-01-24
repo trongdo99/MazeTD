c2b68e2a39ccd367181f5684415a84690471d8cd
Trong Do <trong.do-duc@ubisoft.com>
2024-01-24 18:56:40 +0700

    Continue on Tower Editor

--- Assets/_Project/Code/Editor/TowerEditorWindow.cs
+++ Assets/_Project/Code/Editor/TowerEditorWindow.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c77018dc7ad84c2a69d42c63ae7b537a326e125343d91c99c57a429207846fc
-size 11545
+oid sha256:a8f5f9acc0c19e150013e7eb1080470d1ceda360535f8c5ed4504f2919bf1253
+size 13895
