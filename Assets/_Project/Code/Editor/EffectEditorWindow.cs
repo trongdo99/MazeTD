ea0553005daca33dce12df322155fa5cc24e0a21
Trong Do <trongdoduc99@gmail.com>
2024-01-29 01:08:28 +0700

    Experimenting with UI ToolKit new API

--- Assets/_Project/Code/Editor/EffectEditorWindow.cs
+++ Assets/_Project/Code/Editor/EffectEditorWindow.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7292fe095226958013f72ef4f133b40f67946753ca94232effbee7a0923c6301
-size 14412
+oid sha256:c076df6af5b2ea0119a9d530a82e5e5d3285c02e35d75f18cd09bd60a5741b5e
+size 15534
--- Assets/_Project/Code/ScriptableObjects/BaseStats.cs
+++ Assets/_Project/Code/ScriptableObjects/BaseStats.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f23ee41bb182f65b04877488343119d2bfbc6b5b76f4c642544102076a751c0c
-size 2496
+oid sha256:454ecb49b2291be07fc5532b601ad2df784e7125d26bc0a04bb1cf3d0689cad6
+size 3228
